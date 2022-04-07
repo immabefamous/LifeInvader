79af3219667212184591e5713065fb596ca2a7e7
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-04-07 17:37:02 -0400

    added essientials

---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import Timeline from './Timeline.js'
+import Sidebar from './Sidebar.js'
+const Homepage = () => {
+    return(
+        <div>
+            <h2>Welcome to Lifeinvader</h2>
+            <Timeline />
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Homepage;
