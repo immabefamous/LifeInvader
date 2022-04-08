1db5b0be152d2fe168149c1aa478490152627d3c
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-04-08 14:21:17 -0400

    added title

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,16 @@
-import { useState} from 'react'
+import { useState } from 'react'
 
 const Post = ({post}) => {
-    const [likes, setLikes] =useState(0)
-    return(
-      <div>
-        <img src={post.image} height="250" width="250" />
-        <p>Post by {post.username}</p>
-        <button onClick={() => {setLikes(likes + 1)}}> heart </button>
-        <p>{post.content}</p>
-      </div>
-    )
-  }
+  const [likes, setLikes] = useState(0)
   
-  export default Post;
+  return(
+    <div>
+      <img src={post.image} height="250" width="250" />
+      <p>Post by {post.username} • {likes} likes</p>
+      <button onClick={() => { setLikes(likes + 1)}}>❤️</button>
+      <p>{post.content}</p>
+    </div>
+  )
+}
+
+export default Post;
