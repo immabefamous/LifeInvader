5294d758e7dde987d2933fcc72bc4f7d7b337482
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-04-08 14:39:46 -0400

    edited NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,15 @@ const NavBar = () => {
     return(
         <nav className="navbar">
             <div>
-                <a href="#">LifeInvader</div>
+                <a href="#" className="home-link">LifeInvader</a>
+                </div>
             <div>
             <i class="fa-solid fa-magnifying-glass"></i>
             <input type="search" placeholder="Search" />
             </div>
-            <div><i class="fa-solid fa-bell"></i></div>
+            <div>
+                <i class="fa-solid fa-bell"></i>
+            </div>
         </nav>
     )
 }
