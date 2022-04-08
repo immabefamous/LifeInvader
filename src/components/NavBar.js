64e443006be992e8b1453004eac3af959bdde9c5
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-04-08 14:34:35 -0400

    added NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return(
+        <nav className="navbar">
+            <div>
+                <a href="#">LifeInvader</div>
+            <div>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="search" placeholder="Search" />
+            </div>
+            <div><i class="fa-solid fa-bell"></i></div>
+        </nav>
+    )
+}
+
+export default NavBar;
